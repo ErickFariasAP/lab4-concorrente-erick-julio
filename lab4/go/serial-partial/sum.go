a5b9285b8032cbbe251b84655eaa84cb9833091f
ErickFariasAP <erick.pereira@ccc.ufcg.edu.br>
2024-09-10 15:48:51 -0300

    mudanças na main do serial-partial

--- lab4/go/serial-partial/sum.go
+++ lab4/go/serial-partial/sum.go
@@ -7,28 +7,25 @@ import (
 )
 
 // read a file from a filepath and return a slice of bytes
-func readFile(filePath string) ([]byte, error) {
+func readFile(filePath string) []byte {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		fmt.Printf("Error reading file %s: %v", filePath, err)
-		return nil, err
+		return nil
 	}
-	return data, nil
+	return data
 }
 
 // sum all bytes of a file
-func sum(filePath string) (int, error) {
-	data, err := readFile(filePath)
-	if err != nil {
-		return 0, err
-	}
+func sum(filePath string) int {
+	data := readFile(filePath)
 
 	_sum := 0
 	for _, b := range data {
 		_sum += int(b)
 	}
 
-	return _sum, nil
+	return _sum
 }
 
 func similarity(base []string, target []string) (contador int) {
@@ -58,11 +55,7 @@ func main() {
 	var totalSum int64
 	sums := make(map[int][]string)
 	for _, path := range os.Args[1:] {
-		_sum, err := sum(path)
-
-		if err != nil {
-			continue
-		}
+		_sum := sum(path)
 
 		totalSum += int64(_sum)
 
@@ -74,6 +67,23 @@ func main() {
 	for sum, files := range sums {
 		if len(files) > 1 {
 			fmt.Printf("Sum %d: %v\n", sum, files)
+		}
+	}
+
+	fileFingerprint := make(map[string]int)
+	for _, x := range os.Args[1:] {
+		var finger int
+		finger = sum(x)
+		fileFingerprint[x] = finger
+	}
+
+	for i := 0; i < len(os.Args[1:]); i++ {
+		for j := i + 1; j < len(os.Args[1:]); j++ {
+			file1 := os.Args[i]
+			file2 := os.Args[j]
+			fp1 := fileFingerprint[file1]
+			fp2 := fileFingerprint[file2]
+			similaridade := similarity(fp1, fp2)
 		}
 	}
 }
